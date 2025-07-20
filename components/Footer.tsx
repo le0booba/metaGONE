1f7fc618892ddfc3b380b6bfb887931e4aab910b
Leonid D. <191616590+le0booba@users.noreply.github.com>
2025-07-20 04:44:15 +0300

    upd

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 export const Footer: React.FC = () => (
   <footer className="w-full py-6 border-t border-slate-800 bg-black/20">
     <div className="text-center text-sm text-slate-500">
-      <p>&copy; badrenton, 2025</p>
+      <p>&copy; 2025 metaGONE by badrenton. All rights reserved.</p>
     </div>
   </footer>
 );
